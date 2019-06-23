cb3f286e849c6c0039dbd97c8cc4a12853fd9f47
BernadetteCheng <bernadette.cheng14@gmail.com>
2019-06-23 04:07:51 -0400

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,13 +25,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_transactions);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    Intent intent2 = new Intent(MainActivity.this, Stats.class);
+                    startActivity(intent2);
+                    mTextMessage.setText(R.string.title_stats);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_history);
                     return true;
             }
             return false;
