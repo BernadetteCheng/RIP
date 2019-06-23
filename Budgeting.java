84b359e7ed6a3318d204401d7e770b3cfc7a72fc
KrishdevSutar <43391308+KrishdevSutar@users.noreply.github.com>
2019-06-23 05:06:50 -0400

    Add files via upload
    Pie Chart 2

--- Budgeting.java
+++ Budgeting.java
@@ -6,35 +6,40 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
-import com.razerdp.widget.animatedpieview.AnimatedPieView;
-import com.razerdp.widget.animatedpieview.AnimatedPieViewConfig;
-import com.razerdp.widget.animatedpieview.data.IPieInfo;
-import com.razerdp.widget.animatedpieview.data.PieOption;
-import com.razerdp.widget.animatedpieview.data.SimplePieInfo;
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
 
 public class Budgeting extends AppCompatActivity{
     private Button backBut;
+    int one,two,three,four,tot;
+    double a,b,c,d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budgeting);
-        AnimatedPieView aniPie = findViewById(R.id.pieView);
-        AnimatedPieViewConfig config = new AnimatedPieViewConfig();
-        config.addData(new SimplePieInfo(2500, Color.parseColor("#7fb663"), "Food"));
-        config.addData(new SimplePieInfo(3250, Color.parseColor("#03bfc1"), "Electronics"));
-        config.addData(new SimplePieInfo(4500, Color.parseColor("#a8e5b9"), "Stationary"));
-        config.addData(new SimplePieInfo(5250, Color.parseColor("#e3f467"), "Cleaning"));
-        config.strokeMode(false);
-        config.drawText(true);
-        config.guideLineWidth(8);
-        config.textSize(18);
+        PieChartView pieChartView = findViewById(R.id.chart);
+        List<SliceValue> pieData = new ArrayList<>();
 
 
-        aniPie.applyConfig(config);
-        aniPie.start();
+
+        pieData.add(new SliceValue(15, Color.parseColor("#7fb663")).setLabel("Food"));
+        pieData.add(new SliceValue(25, Color.parseColor("#03bfc1")).setLabel("Electronics"));
+        pieData.add(new SliceValue(10, Color.parseColor("#a8e5b9")).setLabel("Clinical"));
+        pieData.add(new SliceValue(60, Color.parseColor("#e3f467")).setLabel("Stationary"));
+
+        PieChartData pieChartData = new PieChartData(pieData);
+
+        pieChartData.setHasLabels(true);
+        pieChartData.setHasLabels(true).setValueLabelTextSize(18);
+        pieChartView.setPieChartData(pieChartData);
         backBut = (Button) findViewById(R.id.button2);
         backBut.setOnClickListener(new View.OnClickListener() {
             @Override
